758b5a5557b78ef49ce6018693dd3e8edc574812
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2021-07-11 21:24:55 -0700

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'This is a ping commmand!',
     execute(message, args){
         if(message.member.roles.cache.has('849076327058374717')){
-        message.channel.send('pong!');
+        message.channel.send(`:ping_pong: Pong!`);
         }//if
         else{
             message.channel.send('Role does not have command permissions');
