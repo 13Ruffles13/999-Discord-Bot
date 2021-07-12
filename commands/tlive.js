5b2f4173c86dab946e588043fff441274c46de27
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2021-07-11 22:08:49 -0700

    Date

--- commands/tlive.js
+++ commands/tlive.js
@@ -16,6 +16,8 @@ module.exports = {
         .setImage('https://images-ext-2.discordapp.net/external/0nngxg5uEquHyfFHwxPGN7NH2dx3Q3qVCKbbqICJGfE/https/static-cdn.jtvnw.net/jtv_user_pictures/e89d5440-cca5-480d-aa26-e00479d77815-profile_image-300x300.png')
         .setFooter('Make sure to drop a follow');
 
+        timestamp: new Date();
+
         message.channel.send(newEmbed);
         }//if
         else{
