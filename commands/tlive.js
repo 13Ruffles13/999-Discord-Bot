fff7ec2141ff92c1dc656f8f49519db248384aa4
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2021-07-11 22:14:25 -0700

    Time

--- commands/tlive.js
+++ commands/tlive.js
@@ -8,6 +8,7 @@ module.exports = {
         .setTitle('Toni is Live!')
         .setURL('https://m.twitch.tv/tvalentine_')
         .setDescription('Come watch the stream!')
+        timestamp = datetime.now()
         .addFields(
             {name: 'Rule 1', value: 'Be rootin'},
             {name: 'Rule 2', value: 'Be tootin'},
@@ -15,8 +16,6 @@ module.exports = {
         ) //Rule bracket
         .setImage('https://images-ext-2.discordapp.net/external/0nngxg5uEquHyfFHwxPGN7NH2dx3Q3qVCKbbqICJGfE/https/static-cdn.jtvnw.net/jtv_user_pictures/e89d5440-cca5-480d-aa26-e00479d77815-profile_image-300x300.png')
         .setFooter('Make sure to drop a follow');
-
-        timestamp: new Date();
 
         message.channel.send(newEmbed);
         }//if
